ba9e3c834f4d273214f3cac912eba40b95851afd
Thomas Schott <81521884+youalreadyforgot@users.noreply.github.com>
2021-04-21 19:34:56 -0400

    Update mybadshell.c

--- mybadshell.c
+++ mybadshell.c
@@ -6,71 +6,67 @@
 #include <string.h>
 #include <stdbool.h>
 #define MAX_ARG_SIZE 20
+#define NUM_ARGS 10
 #define HOST_NAME_MAX 50
 #define USER_NAME_MAX 50
+#define BUFFER_SIZE 100
 int main()
 {
-    int buffersize_string = 100;	
-    int numOfArgs = 10;
-    int sizeOfArgs = 20;
-    //Write prompt
-    char* hostname = malloc(sizeof(char) * HOST_NAME_MAX + 1);
-    char* user = malloc(sizeof(char) * USER_NAME_MAX);
-    strcpy(user, getenv("USER"));
-    gethostname(hostname, HOST_NAME_MAX + 1);
-    printf("%s@%s:", user, hostname);
-    char *arg = malloc(sizeof(char) * buffersize_string);
-    //Write a fgets that seperates them by spaces
-    fgets(arg, MAX_ARG_SIZE, stdin);
+        while(1){
+	    //Write prompt
+	    char* hostname = malloc(sizeof(char) * HOST_NAME_MAX + 1);
+            char* user = malloc(sizeof(char) * USER_NAME_MAX);
+            strcpy(user, getenv("USER"));
+            gethostname(hostname, HOST_NAME_MAX + 1);
+            printf("%s@%s:", user, hostname);
+            char *arg = malloc(sizeof(char) * BUFFER_SIZE);
+            //Write a fgets that seperates them by spaces
+            fgets(arg, MAX_ARG_SIZE, stdin);
 
-    int argv = strlen(arg);
-    //Test Code for printing number of args
-    //printf("\n%d",argv);
+            int argv = strlen(arg);
+            //Test Code for printing number of args
+            //printf("\n%d",argv);
    
-    // Write a loop to serperate into different strings
-    char *args[sizeOfArgs];
+            // Write a loop to serperate into different strings
+            char *args[MAX_ARG_SIZE];
     
-    for (int i=0;i<numOfArgs;i++)
-    {
-	args[i] = malloc(sizeof(char) * sizeOfArgs);
-    }
-    int cnt = 0;
-    int j = 0;
+            for (int i=0;i<NUM_ARGS;i++)
+            {
+	        args[i] = malloc(sizeof(char) * MAX_ARG_SIZE);
+            }
+            int cnt = 0;
+            int j = 0;
 
-    for(int i=0;i<=strlen(arg);i++)
-        {
-            if(arg[i] == '\n') {arg[i] = '\0';}
-	        if(arg[i] == ' ' || arg[i] == '\0') 
-	        {
+            for(int i=0;i<=strlen(arg);i++)
+            {
+                if(arg[i] == '\n') {
+		    arg[i] = '\0';}
+	        if(arg[i] == ' ' || arg[i] == '\0'){
 		    args[cnt][j] = '\0';
 		    cnt++;
 		    j=0;
-
 		}
 	
-	    else
-		{
-		    args[cnt][j] = arg[i];
-		    j++;
+	    else{
+		args[cnt][j] = arg[i];
+		 j++;
 		}
-	}
-    //Add a NULL to the end so exec can read
-    args[cnt] = NULL;
+	    }
+            //Add a NULL to the end so exec can read
+            args[cnt] = NULL;
     
 
 
-    //for(int i=0;i<cnt;i++)
-    //{
-	//printf("%s", args[i]);
-    //}
-    //Execute the command 
-    bool cont = true;
-    while(cont == true)
-    {
-        execvp(args[0], args);
-    }
-    //Catch any errors from the command stderr
-    //perror("ERROR");
-    //Free up some memory
-    //delete hostname, user, arg, args
+           //for(int i=0;i<cnt;i++)
+           //{
+	       //printf("%s", args[i]);
+           //}
+           //Execute the command 
+           execvp(args[0], args);
+    
+          //Catch any errors from the command stderr
+          //perror("ERROR");
+          //Free up some memory
+          //delete hostname, user, arg, args
+	}
 }
